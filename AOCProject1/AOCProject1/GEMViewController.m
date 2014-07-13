9930cb52e79d1b229642b131e9cc62812f3835d7
eng618 <eng618@fullsail.edu>
2014-07-13 07:21:50 -0400

    Completed second and third vihicle

--- AOCProject1/AOCProject1/GEMViewController.m
+++ AOCProject1/AOCProject1/GEMViewController.m
@@ -16,7 +16,7 @@
 @property (strong, nonatomic) IBOutlet UILabel *vehicleTwoLable;
 @property (strong, nonatomic) IBOutlet UILabel *vehicleTwoDescription;
 @property (strong, nonatomic) IBOutlet UILabel *vehicleThreeLable;
-@property (strong, nonatomic) IBOutlet UILabel *vehicleTreeDescription;
+@property (strong, nonatomic) IBOutlet UILabel *vehicleThreeDescription;
 
 @end
 
@@ -36,6 +36,17 @@
         GEMBaseVehicle *f350 = [GEMVehicleFactory getVehicle:vehicleType_F350];
         self.vehicleOneLable.text = [f350 getVehicle];
         self.vehicleOneDescription.text = [NSString stringWithFormat:@"My favorite modle year is %i, and it gets about %impg", [f350 getModelYear], [f350 getFuleEconomy]];
+        
+        // Instantiate Mustang
+        GEMBaseVehicle *mustang = [GEMVehicleFactory getVehicle:vehicleType_Mustang];
+        self.vehicleTwoLable.text = [mustang getVehicle];
+        self.vehicleTwoDescription.text = [NSString stringWithFormat:@"My favorite modle year is %i, and it gets about %impg", [mustang getModelYear], [mustang getFuleEconomy]];
+        
+        // Instantiate Wrangler
+        GEMBaseVehicle *wrangler = [GEMVehicleFactory getVehicle:vehicleType_Wrangler];
+        self.vehicleThreeLable.text = [wrangler getVehicle];
+        self.vehicleThreeDescription.text = [NSString stringWithFormat:@"My favorite modle year is %i, and it gets about %impg", [wrangler getModelYear], [wrangler getFuleEconomy]];
+        
     
         
         
