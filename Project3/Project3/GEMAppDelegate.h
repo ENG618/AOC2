0c555a6aa6143ed383164b9b2d97ddc6e5b04c0a
eng618 <eng618@fullsail.edu>
2014-07-22 19:57:14 -0400

    Started Project3

---
+++ Project3/Project3/GEMAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  GEMAppDelegate.h
+//  Project3
+//
+//  Created by Eric Garcia on 7/22/14.
+//  Copyright (c) 2014 Garcia Enterprise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GEMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Project3/Project3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Project3
+//
+//  Created by Eric Garcia on 7/22/14.
+//  Copyright (c) 2014 Garcia Enterprise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GEMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GEMAppDelegate class]));
+    }
+}
