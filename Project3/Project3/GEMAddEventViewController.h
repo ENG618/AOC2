a93938e6dd4f0ef787bdf3b2c79c9ccda448af4e
eng618 <eng618@fullsail.edu>
2014-07-22 20:06:32 -0400

    Created main storyboard and required vc's

---
+++ Project3/Project3/GEMAddEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  GEMAddEventViewController.h
+//  Project3
+//
+//  Created by Eric Garcia on 7/22/14.
+//  Copyright (c) 2014 Garcia Enterprise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GEMAddEventViewController : UIViewController
+
+@end
---
+++ Project3/Project3/GEMEventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GEMEventsViewController.h
+//  Project3
+//
+//  Created by Eric Garcia on 7/22/14.
+//  Copyright (c) 2014 Garcia Enterprise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GEMEventsViewController : UIViewController
+
+@end
